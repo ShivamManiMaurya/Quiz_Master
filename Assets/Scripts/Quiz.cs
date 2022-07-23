1e0db56f08e3de2c2fd4182b76168e9a1cd98c94
Shivam Maurya <109658264+ShivamManiMaurya@users.noreply.github.com>
2022-07-23 18:05:54 +0530

    Added some extra functions.
    Added GetNextQusiton() function, SetButtonState(bool state) and some more functionalities.

--- Assets/Scripts/Quiz.cs
+++ Assets/Scripts/Quiz.cs
@@ -2,14 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class Quiz : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI questionBox;
     [SerializeField] QuestionSO question;
     [SerializeField] GameObject[] answerOptions;
+    int correctAnswerIndex;
+    [SerializeField] Sprite defaultAnswerSprite;
+    [SerializeField] Sprite correctAnswerSprite;
+    //bool flag = true;
 
     void Start()
+    {
+        //DisplayQuestions();
+        GetNextQuestion();
+    }
+
+
+    void DisplayQuestions()
     {
         questionBox.text = question.GetQuestion();
 
@@ -19,4 +31,59 @@ public class Quiz : MonoBehaviour
             answerButton.text = question.GetOptions(i);
         }
     }
+
+    public void OnAnswerSelected(int index)
+    {
+        Image buttonImage;
+
+        if (index == question.GetCorrectAnsIndex())
+        {
+            questionBox.text = "Correct!";
+            buttonImage = answerOptions[index].GetComponent<Image>();
+            buttonImage.sprite = correctAnswerSprite;
+        }
+        else
+        {
+            //Change in the questionBox with correct answer as a string.
+            correctAnswerIndex = question.GetCorrectAnsIndex();
+            string correctAnswer = question.GetOptions(correctAnswerIndex);
+            questionBox.text = "Wrong!!...Correct answer is - \n" + correctAnswer;
+
+            //Highlight the correct option by an image if wrong ans is selected.
+            buttonImage = answerOptions[correctAnswerIndex].GetComponent<Image>();
+            buttonImage.sprite = correctAnswerSprite;
+        }
+
+        SetButtonState(false);
+    }
+
+    void SetButtonState(bool state)
+    {
+        for (int i = 0; i < answerOptions.Length; i++)
+        {
+            Button button = answerOptions[i].GetComponent<Button>();
+            button.interactable = state;
+        }
+    }
+
+    void GetNextQuestion()
+    {
+        SetButtonState(true);
+        SetDefaultButtonSprites();
+        DisplayQuestions();
+    }
+
+
+    void SetDefaultButtonSprites()
+    {
+        Image defaultButtonImage;
+
+        for (int i = 0; i < answerOptions.Length; i++)
+        {
+            defaultButtonImage = answerOptions[i].GetComponent<Image>();
+            defaultButtonImage.sprite = defaultAnswerSprite;
+        }
+    }
+
+
 }
