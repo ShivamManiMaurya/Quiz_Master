b37ecfc07a242c23cc69621b91f4438c06ad1f95
Shivam Maurya <109658264+ShivamManiMaurya@users.noreply.github.com>
2022-07-23 14:27:55 +0530

    Options with correct answers

--- Assets/Scripts/QuestionSO.cs
+++ Assets/Scripts/QuestionSO.cs
@@ -7,9 +7,22 @@ public class QuestionSO : ScriptableObject
 {
     [TextArea(2, 6)]
     [SerializeField] string question = "Write your question here";
+    [SerializeField] string[] options = new string[4];
+    [SerializeField] int correctAnsIndex;
 
     public string GetQuestion()
     {
         return question;
     }
+
+    public string GetOptions(int correctAnsIndex)
+    {
+        return options[correctAnsIndex];
+    }
+
+    public int GetCorrectAnsIndex()
+    {
+        return correctAnsIndex;
+    }
+
 }
