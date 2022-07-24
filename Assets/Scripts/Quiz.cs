dcbd749d8ce65c3b6070654a7de127c307e7e5cb
Shivam Maurya <109658264+ShivamManiMaurya@users.noreply.github.com>
2022-07-24 15:03:06 +0530

    Added EndScreen.cs
    EndScreen.cs as the name suggests shows the end screen where we have provided the score and an option to reload the game.

--- Assets/Scripts/Quiz.cs
+++ Assets/Scripts/Quiz.cs
@@ -28,10 +28,17 @@ public class Quiz : MonoBehaviour
     [SerializeField] TextMeshProUGUI scoreText;
     ScoreKeeper scoreKeeper;
 
+    [Header("ProgressBar")]
+    [SerializeField] Slider progressBar;
+
+    public bool isComplete;
+
     void Start()
     {
         timer = FindObjectOfType<Timer>();
         scoreKeeper = FindObjectOfType<ScoreKeeper>();
+        progressBar.maxValue = questions.Count;
+        progressBar.value = 0;
     }
 
     void Update()
@@ -92,6 +99,11 @@ public class Quiz : MonoBehaviour
         SetButtonState(false);
         timer.CancelTimer();
         scoreText.text = ("Score: " + scoreKeeper.CalculateScore() + "%");
+
+        if (progressBar.value == progressBar.maxValue)
+        {
+            isComplete = true;
+        }
     }
 
     void SetButtonState(bool state)
@@ -111,6 +123,7 @@ public class Quiz : MonoBehaviour
             SetDefaultButtonSprites();
             GetRandomQuestion();
             DisplayQuestions();
+            progressBar.value++;
             scoreKeeper.IncrementQuestionsSeen();
         }
     }
